85d0b12d5348d0ffe8f6c1855eb66727af169219
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-05 17:04:26 -0300

    add font for the title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="w-full min-h-screen h-full flex justify-center items-center App">
       <div className="bg-green-600 p-4 min-w-max w-3/12 rounded-lg flex flex-col items-center">
-        <h1 className="text-center align-top">Gifts</h1>
+        <h1 className="text-center text-4xl mb-4 align-top font-MountainsOfChristmas">Gifts</h1>
         <Form addGift={addGift} />
         {gifts.length === 0 ? (
           <p>No gifts yet &#128532;</p>
