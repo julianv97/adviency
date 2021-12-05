f11cc13ce7141777c8c9695145364a2eb1a3c03d
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-05 16:29:47 -0300

    Implementation of customHook useGifts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import useGifts from './customHooks/useGifts';
 import ListOfGifs from './Components/ListOfGifts';
 import Form from './Components/Form';
 import Button from './Components/Shared/Button';
+import './App.css';
+
+const url = 'http://localhost:4000/gifts';
 
 function App() {
-  const [gifts, setGifts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:4000/gifts')
-      .then((response) => response.json())
-      .then((data) => setGifts(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  const addGift = (gift) => {
-    fetch('http://localhost:4000/gifts', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(gift),
-    })
-      .then((response) => response.json())
-      .then((data) => setGifts([...gifts, data]))
-      .catch((err) => console.log(err));
-  };
-
-  const deleteGift = (id) => {
-    fetch(`http://localhost:4000/gifts/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then(() => setGifts(gifts.filter((gift) => gift._id !== id)))
-      .catch((err) => console.log(err));
-  };
-
-  const deleteAllGifts = () => {
-    fetch('http://localhost:4000/gifts', {
-      method: 'DELETE',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then(() => setGifts([]))
-      .catch((err) => console.log(err));
-  };
+  const { gifts, addGift, deleteGift, deleteAllGifts } = useGifts(url);
 
   return (
     <div className="w-full h-screen flex justify-center items-center App">
---
+++ client/src/customHooks/useGifts.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+
+const useGifts = (url) => {
+  const [gifts, setGifts] = useState([]);
+
+  const addGift = (gift) => {
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(gift),
+    })
+      .then((response) => response.json())
+      .then((data) => setGifts([...gifts, data]))
+      .catch((err) => console.log(err));
+  };
+
+  const deleteGift = (id) => {
+    fetch(`${url}/${id}`, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then(() => setGifts(gifts.filter((gift) => gift._id !== id)))
+      .catch((err) => console.log(err));
+  };
+
+  const deleteAllGifts = () => {
+    fetch(url, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then(() => setGifts([]))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setGifts(data))
+      .catch((err) => console.log(err));
+  }, [url]);
+
+  return { gifts, addGift, deleteGift, deleteAllGifts };
+};
+
+export default useGifts;
